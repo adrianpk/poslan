7a4e30830d2ea3eaaa59974fdb3ef738e025f77e
Adrian PK <adrianpk@adrianpk.com>
2019-05-26 11:09:32 +0200

    Package moved.

--- pkg/mailer/amazon/const.go
+++
@@ -1,8 +0,0 @@
-package amazon
-
-const (
-	// This values Eventually can be obtained from config
-	// or the client as appropriate.
-	// Fixed value at the moment for the sake of simplicity (PoC).
-	region = "us-west-2"
-)
